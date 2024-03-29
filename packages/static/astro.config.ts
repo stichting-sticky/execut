b974b1add7d7e53b02520457748484b10cee35dd
Sem van Nieuwenhuizen <leukenaamofzo@gmail.com>
2024-03-29 14:10:55 +0100

    feat: Remove lead scanning since the event is over

--- packages/static/astro.config.ts
+++ packages/static/astro.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config'
 
 // Import Astro integrations
 import icon from 'astro-icon'
-import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
 
@@ -10,12 +9,13 @@ import tailwind from '@astrojs/tailwind'
 export default defineConfig({
   // To ensure that our provider displays `/partners` without trailing slashes output as files instead of directories
   build: { format: 'file' },
+
   integrations: [
     icon({ include: { mdi: ['*'] } }),
-    react(),
     sitemap(),
     tailwind(),
   ],
+
   // The production site will be hosted here
   site: 'https://execut.nl/',
 })
