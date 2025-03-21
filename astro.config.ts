6f68b8eca29f9418765e28ef58b7049ba9b5edba
Semele <hello@semele.fyi>
2025-03-21 15:17:20 +0100

    feat: switch to node adapter

--- astro.config.ts
+++ astro.config.ts
@@ -1,19 +1,27 @@
 import { defineConfig } from 'astro/config'
 
 // Import Astro integrations
-import icon from 'astro-icon'
+import node from '@astrojs/node'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@tailwindcss/vite'
+import icon from 'astro-icon'
 
 // https://astro.build/config
 export default defineConfig({
-  // To ensure that our provider displays `/partners` without trailing slashes output as files instead of directories
-  build: { format: 'file' },
+  // The production site will be hosted here
+  site: 'https://execut.nl/',
+  trailingSlash: 'never',
+  adapter: node({
+    mode: 'standalone',
+  }),
   integrations: [
     icon({ include: { mdi: ['*'] } }),
     sitemap(),
   ],
-  // The production site will be hosted here
-  site: 'https://execut.nl/',
   vite: { plugins: [tailwind()] },
+  // To ensure that our provider displays `/partners` without trailing slashes output as files instead of directories
+  build: { format: 'file' },
+  experimental: {
+    session: true,
+  },
 })
